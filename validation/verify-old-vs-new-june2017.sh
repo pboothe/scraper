f24f0fd85e6081d302b419def6ecbba089ccf8ee
Peter Boothe <pboothe@google.com>
2017-07-31 21:13:13 +0000

    Added comments and clarified logic

--- validation/verify-old-vs-new-june2017.sh
+++ validation/verify-old-vs-new-june2017.sh
@@ -24,31 +24,35 @@ gzdiff ()
   experiment=$4
   oldfile=${olddir}/${filename}
   newfile=${newdir}/${filename}
-  if [[ -e ${newfile}.gz ]] && [[ ! -e ${newfile} ]]
+  # Files in the old archives are uniformly uncompressed. Files in the new
+  # archives, particularly in early June, may or may not be compressed.  If
+  # there is a compressed file but no uncompressed one, then we should
+  # use the file that exists.
+  if [[ -e ${newfile}.gz && ! -e ${newfile} ]]
   then
     newfile=${newfile}.gz
   fi
-  if [[ -f ${oldfile} && -f ${newfile} ]]
+  if [[ ${experiment} == "sidestream" ]]
   then
-    if [[ ${experiment} == "sidestream" ]]
+    # Sidestream files are not compressed
+    lines=$(comm -2 -3 <(sort ${oldfile})  <(sort ${newfile}) | wc -l)
+    if [[ $lines != 0 ]]
     then
-      lines=$(comm -2 -3 <(sort ${oldfile})  <(sort ${newfile}) | wc -l)
-      if [[ $lines != 0 ]]
+      echo FILES DIFFER: ${filename}
+    fi
+  else
+    if ! zdiff -q ${oldfile} ${newfile}
+    then
+      if [[ -e ${newfile}.gz ]]
       then
-        echo FILES DIFFER: ${filename}
-      fi
-    else
-      if ! zdiff -q ${oldfile} ${newfile}
-      then
-        if [[ -e ${newfile}.gz ]]
+        if ! zdiff -q ${oldfile} ${newfile}.gz
         then
-          if ! zdiff -q ${oldfile} ${newfile}.gz
-	  then
-            echo FILES DIFFER: ${filename}
-          fi
-        else
+          # The zipped and unzipped new files both differ from the old file
           echo FILES DIFFER: ${filename}
         fi
+      else
+        # No .gz file exists and the raw new file differs from the old
+        echo FILES DIFFER: ${filename}
       fi
     fi
   fi
@@ -68,7 +72,7 @@ do
       do
         tar xfz ${tgz}
         rm ${tgz}
-        find . | sort > filelist.txt
+        find . -type f | sort > filelist.txt
       done
     popd
     pushd $new
@@ -78,7 +82,7 @@ do
       do
         tar xfz ${tgz}
         rm ${tgz}
-        find . | sort > filelist.txt
+        find . -type f | sort > filelist.txt
       done
     popd
     # Only print out files that are in the legacy but not the new one.  These
